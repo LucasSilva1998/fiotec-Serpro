da0024b24ce361ed523c49ad98b993370ee4e3ed
Lucas Pereira da Silva <lucassilva@fiotec.fiocruz.br>
2025-08-15 11:01:28 -0300

    feat(api): estruturando camadas e documentação do swagger.

---
+++ fiotec-Serpro.API/Extensions/SwaggerExtension.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace fiotec_Serpro.API.Extensions
+{
+    public static class SwaggerExtension
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Fiotec - SERPRO API - Consulta de CPF",
+                    Version = "v1",
+                    Description = "API responsável por consultar informações de CPF utilizando o serviço do SERPRO.",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Lucas Pereira",
+                        Email = "lucassilva@fiotec.fiocruz.com",
+                        Url = new Uri("https://github.com/LucasSilva1998/fiotec-Serpro")
+                    }
+                });
+
+            });
+
+            return services;
+        }
+    }
+}
--- fiotec-Serpro.API/Program.cs
+++ fiotec-Serpro.API/Program.cs
@@ -1,17 +1,24 @@
+using fiotec_Serpro.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger
+builder.Services.AddSwaggerDocumentation();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Fiotec - Serpro API - Consulta de CPF v1");
+        c.RoutePrefix = string.Empty; 
+    });
 }
 
 app.UseAuthorization();
